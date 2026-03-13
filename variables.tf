d94b185c76cf4a586d3c63a4d0f05207869335d2
Max Schaefer <max@excloo.com>
2026-03-13 16:38:48 +1100

    add base64 key

--- variables.tf
+++ variables.tf
@@ -10,8 +10,15 @@ variable "dns_defaults" {
   }
 }
 
+variable "oci_private_key_base64" {
+  description = "OCI private key (base64 encoded)"
+  sensitive   = true
+  type        = string
+}
+
 variable "resend_api_key" {
   description = "Resend API key"
+  sensitive   = true
   type        = string
 }
 
