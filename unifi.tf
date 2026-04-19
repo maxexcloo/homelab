8c22f815c725a3d60d082d23716edc59b67fbb94
Max Schaefer <max@excloo.com>
2026-04-19 18:58:59 +1000

    refactor: uncomment UniFi client list data block and correct client mapping

--- unifi.tf
+++ unifi.tf
@@ -1,11 +1,12 @@
-# data "unifi_client_list" "all" {
-#   wired = true
-# }
+data "unifi_client_list" "all" {
+  wired = true
+}
 
 locals {
   # Placeholder for UniFi client lookup output. When enabled, this should map
   # server keys to UniFi clients so private DNS/IPs can enrich server views.
   unifi_clients = {
-    # for client in data.unifi_client_list.all.clients : client.name => client
+    for client in data.unifi_client_list.all.clients : client.note => client
+    if client.note != null
   }
 }
