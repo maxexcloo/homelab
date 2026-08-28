0061a22fb144906d00217e35806d52d16bd91b08
Max Schaefer <max@excloo.com>
2026-08-28 18:49:21 +1000

    Adopt external service DNS records

--- migrations.tf
+++ migrations.tf
@@ -1,4 +1,14 @@
 import {
+  id = "a531d41ab5c60781d7ab539da748ed00/cd3c10802744fc090567fc0cb8aff628"
+  to = cloudflare_dns_record.managed["excloo.com-manual-CNAME-_acme-challenge.www.reddit"]
+}
+
+import {
   id = "a531d41ab5c60781d7ab539da748ed00/230e706af06c21902a4410a4c5b6c32a"
   to = cloudflare_dns_record.managed["tunnel/hass/home-assistant.excloo.com"]
+}
+
+import {
+  id = "a531d41ab5c60781d7ab539da748ed00/4285f34dc2448fb83eea59ff486de31e"
+  to = cloudflare_dns_record.managed["excloo.com-manual-CNAME-status"]
 }
